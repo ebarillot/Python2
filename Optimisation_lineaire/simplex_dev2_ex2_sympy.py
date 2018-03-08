38d6e8f10e82a4584976ed76bc39ed298c1d91d4
emmanuel_barillot <ebarillot@yahoo.fr>
2018-03-08 18:27:53 +0100

    Devoir2, travaux sur mayavi et sympy

---
+++ Optimisation_lineaire/simplex_dev2_ex2_sympy.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+
+from __future__ import print_function
+import sympy as sp
+# from sympy import *
+
+sp.init_printing(use_unicode=True)
+
+x, y, z, lamb = sp.symbols('x y z lamda')
+
+M = sp.Matrix(3,3,[2,-1,2,2,-3,1,-1,1,-2])
+print('M: ', end='')
+print(M)
+
+V = sp.Matrix(3,1,[x,y,z])
+print('V: ', end='')
+print(V)
+
+# b = sp.Matrix([[4],[-5],[-1]])
+b = sp.Matrix([[4,-5,-1]]).transpose()
+print('b: ', end='')
+print(b)
+
+detM = sp.det(M)
+detMf = sp.factor(detM)
+print('detM: ', end='')
+print(detM)
+print('detMf: ', end='')
+print(detMf)
+
+rsetM = sp.linsolve((M,b),[x,y,z])
+print('rsetM: ', end='')
+print(rsetM)
+
+
+# print((M**-1)*b)
+
+print('M: '+sp.latex(M))
+print('V: '+sp.latex(V))
+print('b: '+sp.latex(b))
+print('detM: '+sp.latex(detM))
+print('detMf: '+sp.latex(detMf))
+
+
+print('M: '+sp.latex(M))
+print('systM: '+sp.latex(M*V-b))
+print('rsetM: '+sp.latex(rsetM))
+
+
+
+# solution numérique
+from scipy.optimize import linprog
+c = [-4, -5]        # attention, linprog cherche à minimiser et non as à maximiser
+A = [[3, 1], [1, 4]]
+b = [1, 1]
+x0_bnds = (0, None)
+x1_bnds = (0, None)
+res = linprog(c, A, b, bounds=(x0_bnds, x1_bnds))
+print(res)
